199e16436bf7bf8744853cf861f6d8a0ef9584a6
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-12 18:04:04 -0500

    Added login component

--- src/StockUp.js
+++ src/StockUp.js
@@ -2,25 +2,24 @@ import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
-// import { Login } from "./auth/Login"
-// import { Register } from "./auth/Register"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const StockUp = () => (
     <>
         <Route render={() => {
-            // if (localStorage.getItem("lu_token")) {
+            if (localStorage.getItem("lu_token")) {
                 return <>
                     <Route render={NavBar} />
-                    {/* <Route render={props => <ApplicationViews {...props} />} /> */}
+                    <Route render={props => <ApplicationViews {...props} />} />
                 </>
             } 
-        // else {
-        //         return <Redirect to="/login" />
-        //     }
-        // }
-      } />
+        else {
+                return <Redirect to="/login" />
+            }
+        }} />
 
-        {/* <Route path="/login" render={Login} />
-        <Route path="/register" render={Register} /> */}
+        <Route path="/login" render={Login} />
+        <Route path="/register" render={Register} />
     </>
 )
