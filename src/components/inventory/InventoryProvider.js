0c5fe819e345959b800f18eb766510710ad6ab0c
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-13 16:25:42 -0500

    Added addToInventory function in InventoryProvider

---
+++ src/components/inventory/InventoryProvider.js
@@ -0,0 +1,24 @@
+import React, { createContext } from 'react'
+
+export const InventoryContext = createContext()
+
+export const InventoryProvider = (props) => {
+    const [inventory, setInventory] = useState([])
+
+    const addToInventory = (part) => {
+        return fetch("http://localhost:8000/inventory", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            },
+            body: JSON.stringify(part)
+         })
+    }
+
+    return (
+        <InventoryContext.Provider value={{ inventory, addToInventory }}>
+            {props.children}
+        </InventoryContext.Provider>
+    )
+}
