b86143b89df7b288a82b3ea1b24858204315a102
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-15 18:15:09 -0500

    returned list of products linked to product detail

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,8 @@ import { InventoryForm } from "./inventory/InventoryForm"
 import { InventoryProvider } from "./inventory/InventoryProvider"
 import { DatabaseList } from "./parts/DatabaseList"
 import { DatabaseProvider } from "./parts/DatabaseProvider"
+import { ProductList } from "./products/ProductList"
+import { ProductProvider } from "./products/ProductProvider"
 
 export const ApplicationViews = () => {
     return <>
@@ -13,12 +15,17 @@ export const ApplicationViews = () => {
         }}>
             <DatabaseProvider>
                 <InventoryProvider>
-                    <Route path='/database'>
-                        <DatabaseList/>
-                    </Route>
-                    <Route path='/inventory/new'>
-                        <InventoryForm/>
-                    </Route>
+                    <ProductProvider>
+                        <Route path='/database'>
+                            <DatabaseList/>
+                        </Route>
+                        <Route path='/inventory'>
+                            <ProductList/>
+                        </Route>
+                        <Route path='/inventory/new'>
+                            <InventoryForm/>
+                        </Route>
+                    </ProductProvider>
                 </InventoryProvider>
             </DatabaseProvider>
 
--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,11 +1,27 @@
 import React, { useContext, useEffect } from 'react' 
+import { Link, useHistory } from 'react-router-dom'
 import { ProductContext } from './ProductProvider'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
 
 export const ProductList = () => {
+    const history = useHistory()
+    
     const {products, getProducts} = useContext(ProductContext)
 
     useEffect(() => {
         getProducts()
     }, [])
-    return(<></>)
+    
+    return(<>
+        <h1>Products</h1>
+        <Button variant='success'>Add New Product</Button>
+        {products.map(product => 
+            <ListGroup horizontal key={product.id}>
+                <ListGroup.Item variant='light'><Link to={{pathname: `/products/${product.id}`}}>{product.name}</Link></ListGroup.Item>
+                <Button variant='danger'>Delete</Button>
+            </ListGroup>
+        )}
+    </>)
 }
