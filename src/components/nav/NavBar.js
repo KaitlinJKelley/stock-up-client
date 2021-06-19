1ac5b519d8b08ec4b963caa2540ba2d51f6fd2c2
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-19 18:45:25 -0500

    Refactored newOrderRecAllowed function in NavBar to actually work

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -19,14 +19,14 @@ export const NavBar = (props) => {
   const newOrderRecAllowed = () => {
     getRecentOrderRec()
     .then(res => {
-      if (res.date_received) {
-        history.push('/recs/new')
+      for (let i=0; i < res.orderrecpart_set.length; i++) {
+        if (res.orderrecpart_set[i].date_received === null) {
+          setShow(true)
+          return(<></>)
+        }
       }
-      else {
-        setShow(true)
-      }
+      history.push('/recs/new')
     })
-
   }
 
    return ( 
