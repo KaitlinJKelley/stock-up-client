3f0abf59c0b363efc3367fcfff76c089624ba2a7
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-19 16:43:20 -0500

    Added functionality to NavBar.js to tell user to close open order rec before starting a new one or push to recs/new if no recs are open

--- src/components/StockUp.js
+++ src/components/StockUp.js
@@ -1,24 +1,60 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
-import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
+import { OrderRecProvider } from "./orderRecs/OrderRecProvider"
+import { NavBar } from "./nav/NavBar"
+
 // import { Register } from "./auth/Register"
+
+// export const StockUp = (props) => (
+//     <>
+
+//         <Route render={() => {
+//             if (localStorage.getItem("lu_token")) {
+//                 return <>
+//                     <Route render={props => <ApplicationViews {...props} />} />
+//                 </>
+//             } else {
+//                 return <Redirect to="/login" />
+//             }
+//         }} />
+
+//         <Route path="/login" render={Login} />
+//         {/* <Route path="/register" render={Register} /> */}
+//     </>
+// )
 
 export const StockUp = () => (
     <>
         <Route render={() => {
             if (localStorage.getItem("lu_token")) {
                 return <>
-                    <Route render={NavBar} />
-                    <Route render={props => <ApplicationViews {...props} />} />
+                    <OrderRecProvider>
+                        <NavBar />
+                    <ApplicationViews />
+                    </OrderRecProvider>
                 </>
             } else {
                 return <Redirect to="/login" />
             }
         }} />
 
-        <Route path="/login" render={Login} />
-        {/* <Route path="/register" render={Register} /> */}
+        <Route path="/login" render={() => {
+            if (localStorage.getItem("lu_token")) {
+                return <Redirect to="/" />
+            } else {
+                return <Login />
+            }
+        }} />
+
+        <Route path="/register" render={() => {
+            if (localStorage.getItem("lu_token")) {
+                return <Redirect to="/" />
+            } else {
+                // return <Register />
+                return("")
+            }
+        }} />
     </>
 )
