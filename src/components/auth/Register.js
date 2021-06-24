dcdd13ef87b1190fbe7f36c9b8741c834ab866b8
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-24 10:34:05 -0500

    replaced login and register urls

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -29,7 +29,7 @@ export const Register = () => {
                 "employeeId": employeeId.current.value
             }
 
-            return fetch("http://127.0.0.1:8000/register", {
+            return fetch("https://stockupapi.herokuapp.com/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
